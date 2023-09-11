6891cd05495e29326f4c67ce65632f36761aa75b
Sumit <sumit223312@gmail.com>
2023-09-11 23:51:30 +0530

    update Code

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path")
+require("./config/database").connect();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+
+
+
+//User routes
+const userRouter = require("./routes/testRoutes");
+app.use("/test", userRouter);
+
+const testTypeRouter = require("./routes/testTypeRoutes");
+app.use("/test-type", testTypeRouter);
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
